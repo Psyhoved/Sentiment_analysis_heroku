083295bd76000bcd85315c9839abbc2a88d5e075
Alexandr Lebedev <mr.alexandrlebedev@mail.ru>
2022-05-16 13:37:04 +0300

    add test_sentiment_API

--- test_sentiment_API.py
+++ test_sentiment_API.py
@@ -3,7 +3,28 @@ from sentiment_model_API import app
 
 client = TestClient(app)
 
+
 def test_read_sentiment_model_API():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_read_predict_positive():
+    response = client.post("/predict/",
+                           json={"text": "Я люблю машинное обучение!"}
+                           )
+    json_data = response.json()
+
+    assert response.status_code == 200
+    assert json_data['label'] == 'POSITIVE'
+
+
+def test_read_predict_negative():
+    response = client.post("/predict/",
+                           json={"text": "Я ненавижу машинное обучение!"}
+                           )
+    json_data = response.json()
+
+    assert response.status_code == 200
+    assert json_data['label'] == 'NEGATIVE'
